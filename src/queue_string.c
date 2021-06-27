c0455d6ceff4f38343108e90f6ab8c5a4f6e5db1
Mateusz Kmak <hamsterkmak@gmail.com>
2021-06-27 16:33:33 +0200

    changed queue_string to use FAM instead of list and deleted list_string

--- src/queue_string.c
+++ src/queue_string.c
@@ -1,26 +1,25 @@
-#include <list_string.h>
 #include <queue_string.h>
 #include <semaphore.h>
 #include <stdlib.h>
 #include <threads.h>
+#include <string.h>
 
 struct queue_string {
   mtx_t mutex; // used to allow only one enqueue or dequeue operation at the
                // same time
   sem_t semaphore_dequeue; // used to keep track of number of elements in queue
   sem_t semaphore_enqueue; // and allow exclusive access to each of them
-  list_string *baseList;
   unsigned int capacity;
   unsigned int curSize;
+  char* baseArray[]; //FAM
 };
 
 queue_string *queue_string_create(unsigned int num) {
-  queue_string *toReturn = malloc(sizeof(queue_string));
+  queue_string *toReturn = malloc(sizeof(queue_string) + num * sizeof(char*));
 
   if (toReturn) {
     toReturn->capacity = num;
     toReturn->curSize = 0;
-    toReturn->baseList = list_string_create();
     mtx_init(&(toReturn->mutex), mtx_plain);
     sem_init(&toReturn->semaphore_dequeue, 0, toReturn->curSize);
     sem_init(&toReturn->semaphore_enqueue, 0, num);
@@ -29,7 +28,6 @@ queue_string *queue_string_create(unsigned int num) {
 }
 
 void queue_string_destroy(queue_string *ptr) {
-  list_string_destroy(ptr->baseList);
   mtx_destroy(&ptr->mutex);
   sem_destroy(&ptr->semaphore_dequeue);
   sem_destroy(&ptr->semaphore_enqueue);
@@ -39,7 +37,7 @@ void queue_string_destroy(queue_string *ptr) {
 void queue_string_enqueue(queue_string *ptr, char *string) {
   sem_wait(&ptr->semaphore_enqueue);
   mtx_lock(&ptr->mutex);
-  list_string_append(ptr->baseList, string);
+  ptr->baseArray[ptr->curSize] = string;
   ptr->curSize++;
   mtx_unlock(&ptr->mutex);
   sem_post(&ptr->semaphore_dequeue);
@@ -48,9 +46,9 @@ void queue_string_enqueue(queue_string *ptr, char *string) {
 char *queue_string_dequeue(queue_string *ptr) {
   sem_wait(&ptr->semaphore_dequeue);
   mtx_lock(&ptr->mutex);
-  char *toReturn;
-  list_string_popFirst(ptr->baseList, &toReturn);
+  char *toReturn = ptr->baseArray[0];
   ptr->curSize--;
+  memmove(ptr->baseArray, &ptr->baseArray[1], sizeof(char*) * ptr->curSize);
   mtx_unlock(&ptr->mutex);
   sem_post(&ptr->semaphore_enqueue);
   return toReturn;
